0ef9c8fc02c06523248d7fe6401864e48d95e8e7
Caleb Greer <calebgreer18@gmail.com>
2019-04-02 22:33:28 -0400

    Part 83 Completed

--- EscapeGame/Intermediate/Build/Win64/UE4Editor/Inc/EscapeGame/EscapeGame.init.gen.cpp
+++ EscapeGame/Intermediate/Build/Win64/UE4Editor/Inc/EscapeGame/EscapeGame.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeEscapeGame_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/EscapeGame",
 				PKG_CompiledIn | 0x00000000,
-				0x406E7FB1,
-				0x41AFAB86,
+				0xB73BAA29,
+				0x8FB0E6C0,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
--- EscapeGame/Source/EscapeGame/PositionReporter.cpp
+++ EscapeGame/Source/EscapeGame/PositionReporter.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReporter.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReporter::UPositionReporter()
@@ -18,8 +19,9 @@ void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on Chair!"));
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
 	
 
 }
